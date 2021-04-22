06c0ab4e6a06db22dd3eeec6088ca2ab5f4f67b7
rynnchan <ogw334@gmail.com>
2021-04-23 01:35:13 +0900

    commit

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,8 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            MapView()
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -70)
+                .padding(.bottom, -60)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    
+                    Spacer()
+                    
+                    Text("California")
+                        .font(.subheadline)
+                }
+            }
             .padding()
+            
+            Spacer()
+        }
     }
 }
 
